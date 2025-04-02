9d8fe27cd20c79056e6cee08d49a4df64f5f1168
Pratham Siddhpura <prathamsidd19@gmail.com>
2025-04-02 21:48:49 +0530

    -- Updating services and billing

--- services/purchase_orders.js
+++ services/purchase_orders.js
@@ -60,4 +60,65 @@ async function deleteBulkPurchaseOrders(shipment_id,transaction) {
           throw err;
      }
 }
-module.exports={createBulkPurchaseOrders,deleteBulkPurchaseOrders};
+async function createPurchaseOrders(purchase_order_details) {
+     const transaction = await sequelize.transaction()
+     try{
+          const purchase_order= await purchase_orders.create(purchase_order_details,{transaction});
+          await products.update(
+               {
+                    avg_cost_price: sequelize.literal(`((avg_cost_price)*(quantity) + ${purchase_order.product_quantity}*${purchase_order.unit_price})/(quantity + ${purchase_order.product_quantity})`),
+                    quantity: sequelize.literal(`quantity + ${purchase_order.product_quantity}`)
+               },
+               {
+                 where: {
+                   id: purchase_order.product_id,
+                 },
+                 transaction,
+               }
+             )
+          await transaction.commit();
+          return purchase_order;
+     }catch(err)
+     {
+          await transaction.rollback();
+          throw err;
+     }
+}
+
+async function updatePurchaseOrderById(purchase_order_details,id) {
+     const transaction=await sequelize.transaction();
+    try{
+      const purchase_order=await purchase_orders.findByPk(id);
+      const product=await products.findByPk(purchase_order_details.product_id)
+      const current_avg_price=product.avg_cost_price;
+      const current_quantity=product.quantity;
+      if (current_quantity - purchase_order.product_quantity <= 0) {
+          throw new Error("Invalid quantity calculation: current stock is less than or equal to the removed quantity");
+      }
+      const old_avg_price=((current_avg_price*current_quantity) - (purchase_order.unit_price)*(purchase_order.product_quantity))/(current_quantity-purchase_order.product_quantity);
+      console.log("old_avg_price",old_avg_price);
+      const new_avg_price=((old_avg_price*(current_quantity-purchase_order.product_quantity)) + (purchase_order_details.unit_price)*(purchase_order_details.product_quantity))/(current_quantity-purchase_order.product_quantity + purchase_order_details.product_quantity)
+      console.log("new_avg_price",new_avg_price);
+      await products.update(
+        {
+          quantity: sequelize.literal(`quantity - ${purchase_order.product_quantity} + ${purchase_order_details.product_quantity}`),
+          avg_cost_price: new_avg_price
+        },{
+          where: {
+            id:purchase_order_details.product_id,
+          },
+          transaction,
+        }
+      )
+      await purchase_orders.update(purchase_order_details,{where:{id:id},transaction});
+      await transaction.commit();
+      const updated_purchase_order=await purchase_orders.findByPk(id);
+      return updated_purchase_order;
+    }
+    catch(err)
+    {
+      await transaction.rollback();
+      throw err;
+    }
+}
+module.exports={createBulkPurchaseOrders,deleteBulkPurchaseOrders,createPurchaseOrders,updatePurchaseOrderById};
